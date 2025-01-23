1147173391be9d91490fb2e2176ec8afafb53a4e
AdelleStuuu <adelledoesstuff@gmail.com>
2025-01-23 20:01:08 +0800

    router addition

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
     console.log('Server started on port 3000');  
 
-    res.render("index");
+    res.render("index", {text : 'World'});
 });
+
+const userRouter = require('./routes/users');
+
+app.use('/users', userRouter);
 
 app.listen(3000);

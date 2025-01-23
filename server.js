d1159573023e5fcda112ebc0be009da47a799ca7
AdelleStuuu <adelledoesstuff@gmail.com>
2025-01-23 19:35:23 +0800

    new library

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express(); 
 
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-    console.log('Server started on port 3000');  // Log the server start message to the console
-    res.render("index.html")
+    console.log('Server started on port 3000');  
+
+    res.render("index");
 });
 
 app.listen(3000);

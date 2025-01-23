ba6362eb896a0655c137caf0be1d8e042c9d59ba
AdelleStuuu <adelledoesstuff@gmail.com>
2025-01-23 19:34:46 +0800

    added ejs file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', function(req, res) {
     console.log('Server started on port 3000');  // Log the server start message to the console
-    res.send('Hello, World!');
+    res.render("index.html")
 });
 
 app.listen(3000);
